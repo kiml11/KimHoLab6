6f8336bcca26f0422bd2568ee5e1797e65c2aec1
DanielleSamaroo <127218540+DanielleSamaroo@users.noreply.github.com>
2023-03-22 23:11:38 -0400

    I changed a few thing i think it works. I also left comments in the code.

--- lab6.py
+++ lab6.py
@@ -15,17 +15,26 @@ def encoder(password):
         encoded_password = encoded_password + str(temp_pass)
     return encoded_password
 
+# Code written by Danielle Samaroo
+def decode(encoded_password):
+    decode_password = "".join([str((int(digit) - 3) % 10) for digit in encoded_password])
+    return decode_password
+
 
 if __name__ == '__main__':
+    encoded_password = ""
     while True:
         print_menu()
         option = int(input("\nPlease enter an option: "))
         if option == 1:
             password = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!\n")
-            print(encoder(password))
+            # I changed this because the first option is not supposed to print the password only store it
+            #print(encoder(password))
+            encoded_password = encoder(password)
         elif option == 2:
-            password = input("Please enter your password to decode: ")
-            print("The encoded password is, and the original password is \n")
+            # I removed this because you only needed one password input for both options
+            #password = input("Please enter your password to decode: ")
+            print(f"The encoded password is{encoded_password}, and the original password is {password}.\n")
         elif option == 3:
             break
